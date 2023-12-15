74a368ad6f6fa73eda4cfdc1fc002505450c3854
KIRINn5 <141363578+KIRINn5@users.noreply.github.com>
2023-12-15 21:16:38 +0200

    first-commit

---
+++ js/task-04.js
@@ -0,0 +1,25 @@
+const form = document.querySelector('form.login-form');
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  
+  const inputs = form.elements;
+  let data = {};
+  
+  for (let i = 0; i < inputs.length; i++) {
+    const input = inputs[i];
+    
+    if (input.tagName === 'INPUT' && input.value.trim() === '') {
+      alert('All form fields must be filled in');
+      return;
+    }
+    
+    data[input.name] = input.value.trim();
+  }
+  
+  console.log(data);
+  form.reset();
+}
+
+console.log("task-4");
